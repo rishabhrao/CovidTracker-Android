a896e82bfd0b14eb5cdaa98adfce4f817563d671
rishabhrao <admin@rishabhrao.tk>
2020-08-15 00:17:09 +0530

    Added CovidTracker-Android-Java

---
+++ app/src/main/java/rishabhrao/covidtracker/MainActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2020 Rishabh Rao.
+ * All Rights Reserved.
+ */
+
+package rishabhrao.covidtracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_summary, R.id.navigation_help)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
